ac1ceb7a24fd233dad4383a3a689043f47ac25a3
Juan Carlos <gurquiza8@gmail.com>
2024-05-29 14:23:41 -0600

    Diferentes respuestas

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
+import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
 
     console.log(req.url);
 
-    res.write('Hola mundo');
-    res.end();
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write('<h1>Hola mundo!</h1>');
+    // res.end();
+
+    // const data = { name: 'John Doe', age: 30, city: 'New York' };
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.end( JSON.stringify(data) );
+
+    if ( req.url === '/' ) {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end( htmlFile );
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end();
+    }
     
 });
 
