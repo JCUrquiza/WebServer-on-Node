1d33833c5f5d25d96e7c3f897e4f55360f56a86f
Juan Carlos <gurquiza8@gmail.com>
2024-05-29 14:03:08 -0600

    First commit

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+
+    console.log(req.url);
+
+    res.write('Hola mundo');
+    res.end();
+    
+});
+
+
+
+server.listen(8080, () => {
+
+    console.log('Server running on port 8080');
+
+});
+
+
